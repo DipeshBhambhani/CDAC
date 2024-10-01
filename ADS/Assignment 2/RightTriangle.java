c2bb4626b0cc440a5db7f4c896454f0e00eff6d8
DipeshBhambhani <47105457+DipeshBhambhani@users.noreply.github.com>
2024-10-01 09:05:17 +0530

    Add files via upload

--- ADS/Assignment 2/RightTriangle.java
+++ ADS/Assignment 2/RightTriangle.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 public class RightTriangle{
 	public static void main(String[] args) {
-		
-		int n = 3;
+		System.out.println("Enter a number");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();;
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<= i; j++){
 				System.out.print("* ");
